e70d89ed941ffd84d41734d35706f70fe43775ad
Nika Katsadze <nika.katsadze75@gmail.com>
2025-08-24 20:43:07 +0400

    completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const reversed = str.split('').reverse().join('')
+    if (reversed == str) {
+        return true
+    } else {
+        return false
+    }
 
 };
+
+console.log(palindromes('please help'))
+console.log(palindromes('pApAp'))
+
 
 // Do not edit below this line
 module.exports = palindromes;
